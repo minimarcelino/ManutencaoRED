724162e2973243f73de023102c424153eb933a44
IgorMTeixeira <igorteixeirapf@hotmail.com>
2023-10-25 17:29:05 -0300

    tentando mexer no curso do red

--- Front-End/src/app/cra/processo-red/processo-red.component.ts
+++ Front-End/src/app/cra/processo-red/processo-red.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { alunoService } from 'src/app/services/alunos.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { cursoService } from 'src/app/services/cursos.service';
 
 @Component({
   selector: 'app-processo-red',
   templateUrl: './processo-red.component.html',
   styleUrls: ['./processo-red.component.css']
 })
-export class ProcessoREDComponent {
-  constructor(private router: Router) { }
+export class ProcessoREDComponent implements OnInit {
+  constructor(private router: Router, private alunoservice: alunoService, private cursoservice: cursoService) { }
+
+
+
+  alunos: any[] = [];
+  cursos: any[] = [];
+  filtredCursos: any[] = [];
+  cadastrarRed!: FormGroup;
+
+
+  ngOnInit(): void {
+    this.cadastrarRed = new FormGroup({
+      aluno: new FormControl('', [Validators.required]),
+      curso: new FormControl('', [Validators.required]),
+      afastamento: new FormControl('', [Validators.required]),
+      periodo_inicio: new FormControl('', [Validators.required]),
+      periodo_fim: new FormControl('', [Validators.required]),
+    });
+    this.fetchAlunos();
+  }
 
   displayFn(aluno: any): string {
-    return aluno && aluno.nome;
+    let pront = aluno.prontuario;
+    let nome = aluno.nome;
+    let pront_aluno = pront + ' - ' + nome;
+    console.log(pront_aluno);
+    return aluno && pront_aluno;
+
   }
+
 
   InputFile(event: any) {
 
@@ -23,9 +50,45 @@ export class ProcessoREDComponent {
     }
   }
 
-  teste(){
+  async fetchAlunos() {
+    const response = await this.alunoservice.getAluno();
+    this.alunos = response.data.alunos;
+  }
+
+
+  async fetchCursos() {
+    let aluno_curso = this.alunos;
+    console.log(aluno_curso);
+    const response = await this.cursoservice.getCursos();
+    this.cursos = response.data.cursos;
+   // this.filtredCursos = this.cursos.filter(curso => curso.idcurso ===  );
+    console.log(this.filtredCursos);
+    console.log(this.cursos);
+  }
+
+  teste() {
     this.router.navigate(['/cra'])
     console.log("funcionou");
   }
-  
+
+  get aluno() {
+    return this.cadastrarRed.get('aluno')!.value;
+  }
+
+  get curso() {
+    return this.cadastrarRed.get('curso')!.value;
+  }
+
+  get periodo_inicio() {
+    return this.cadastrarRed.get('periodo_inicio')!.value;
+  }
+
+  get periodo_fim() {
+    return this.cadastrarRed.get('periodo_fim')!.value;
+  }
+
+  get afastamento() {
+    return this.cadastrarRed.get('afastamento')!.value;
+  }
+
 }
