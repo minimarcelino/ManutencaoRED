4efae330c86393be66b7898fa57dacb5eb2b3117
IgorMTeixeira <igorteixeirapf@hotmail.com>
2023-11-20 04:11:51 -0300

    visualizando reds de cada curso

--- Front-End/src/app/coordenador/visualizacaoRed/listar/listar.component.ts
+++ Front-End/src/app/coordenador/visualizacaoRed/listar/listar.component.ts
@@ -11,7 +11,7 @@ import { messageDialog } from 'src/app/services/messageDialog.service';
 import { redService } from 'src/app/services/red.service';
 import { SnackBarComponent } from 'src/app/utils/snack-bar/snack-bar.component';
 import { VisualizarComponent } from '../visualizar/visualizar.component';
-import { curso } from 'src/app/modelo/curso';
+
 
 
 export interface aluno {
@@ -23,6 +23,13 @@ export interface aluno {
   email: String;
   curso: curso;
 }
+
+export interface curso {
+  idcurso: number;
+  sigla: string;
+
+}
+
 
 export interface red {
   idRED: number,
@@ -47,18 +54,22 @@ export class ListarRedComponent implements OnInit {
 
   alunos: any[] = [];
   reds: any[] = [];
-
+  filteredReds: any[] = [];
+  cursos: curso[] = [];
   dataSource: any;
-  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  selectedCurso = '';
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   displayedColumns = ['Nome', 'Curso', 'Início RED', 'Tempo Afastamento', 'Previsão Término', 'Situação', 'Ações'];
 
   constructor(private snackBar: MatSnackBar, private router: Router, public dialogQuestionService: messageDialog, private alunoservice: alunoService,
     private dialog: MatDialog, private redService: redService) {
+    this.filteredReds = [];
   }
 
   ngOnInit(): void {
     this.findAll()
+
   }
 
   applyFilter(data: Event) {
@@ -67,13 +78,47 @@ export class ListarRedComponent implements OnInit {
   }
 
 
-  async findAll() {
-    const response = await this.redService.getRed();
-    this.reds = response.data.reds;
-    this.dataSource = new MatTableDataSource<any>(this.reds);
-    this.dataSource.paginator = this.paginator;
+  filterByCurso(event: MatSelectChange) {
+    const selectedCurso = event.value;
 
+    if (selectedCurso === '') {
+      // Se o curso selecionado for '', remove a filtragem
+      this.filteredReds = this.reds;
+    } else {
+      // Caso contrário, filtra os REDs por curso
+      this.filteredReds = this.reds.filter(a => a.aluno.curso.sigla === selectedCurso);
+    }
+
+    // Atualiza o dataSource com os REDs filtrados
+    this.dataSource = new MatTableDataSource<any>(this.filteredReds);
+    this.dataSource.paginator = this.paginator;
   }
+
+
+ async findAll() {
+  const response = await this.redService.getRed();
+  this.reds = response.data.reds;
+  this.dataSource = new MatTableDataSource<any>(this.reds);
+  this.dataSource.paginator = this.paginator;
+
+  // Cria um conjunto para armazenar cursos únicos
+  const uniqueCursos = new Set<number>();
+
+  this.reds.forEach(red => {
+    uniqueCursos.add(red.aluno.curso.idcurso);
+  });
+
+  // Converte o conjunto de IDs de curso de volta para um array de cursos
+  this.cursos = Array.from(uniqueCursos).map(cursoId => this.reds.find(red => red.aluno.curso.idcurso === cursoId)?.aluno.curso);
+
+  // Filtra cursos nulos (pode ocorrer se o curso não for encontrado)
+  this.cursos = this.cursos.filter(curso => curso !== undefined);
+
+  // Log para depuração
+  console.log('Cursos:', this.cursos);
+}
+
+
   formatData(data: Date): string {
     if (data) {
       return formatDate(data, 'dd/MM/yyyy', 'en-US', 'UTC');
