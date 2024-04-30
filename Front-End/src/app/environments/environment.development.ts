8f0c2bd1fbdf764b647b9b2761c78067e55d533a
murilofray <murilofray01@gmail.com>
2024-04-30 05:56:22 -0300

    Refatorando emails/Colocando /servidor no environment localhost

--- Front-End/src/app/environments/environment.development.ts
+++ Front-End/src/app/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //API: 'http://localhost:3333/api/'
+  //API: 'http://localhost:3333/servidor/'
   API:'http://red.pep2.ifsp.edu.br/servidor/'
 };
